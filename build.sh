c159a4abbbdd584b22580671624977f206665f06
vwvw <vwvw@users.noreply.github.com>
2024-04-19 14:43:58 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,14 +5,5 @@
 # install HexType source codes
 ./scripts/install-hextype-files.sh
 
-# build HexType
-case "$(uname -s)" in
-  Darwin)
-  corecount="$(sysctl -n hw.ncpu)"
-  ;;
-  *)
-  corecount="$(`grep '^processor' /proc/cpuinfo|wc -l`)"
-  ;;
-esac
 
 make -j"$corecount"
